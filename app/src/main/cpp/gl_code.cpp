4bc034ca8606c256ea00fa2da4d262109a8e5024
tanyubo <tanyubo@lixiang.com>
2023-02-13 16:18:57 +0800

    Initial commit

---
+++ app/src/main/cpp/gl_code.cpp
@@ -0,0 +1,250 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// OpenGL ES 2.0 code
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <GLES3/gl32.h>
+#include <GLES2/gl2ext.h>
+#include <android/log.h>
+#include <jni.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define LOG_TAG "libgl2jni"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+
+
+static void printGLString(const char* name, GLenum s) {
+  const char* v = (const char*)glGetString(s);
+  LOGI("GL %s = %s\n", name, v);
+}
+
+static void checkGlError(const char* op) {
+  for (GLint error = glGetError(); error; error = glGetError()) {
+    LOGI("after %s() glError (0x%x)\n", op, error);
+  }
+}
+
+auto gVertexShader =
+    "#version 300 es\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec2 aTexCoord;\n"
+    "out vec2 TexCoord;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+    "}\n";
+
+
+auto gFragmentShader =
+    "#version 300 es\n"
+    "precision mediump float;\n"
+    "out vec4 FragColor;\n"
+    "in vec2 TexCoord;\n"
+    "uniform sampler2D ourTexture;\n" // 在OpenGL程序代码中设定这个变量
+    "void main()\n"
+    "{\n"
+    "   FragColor = texture(ourTexture, TexCoord);\n"
+    "}\n\0";
+
+
+GLuint loadShader(GLenum shaderType, const char* pSource) {
+  GLuint shader = glCreateShader(shaderType);
+  if (shader) {
+    glShaderSource(shader, 1, &pSource, NULL);
+    glCompileShader(shader);
+    GLint compiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (!compiled) {
+      GLint infoLen = 0;
+      glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+      if (infoLen) {
+        char* buf = (char*)malloc(infoLen);
+        if (buf) {
+          glGetShaderInfoLog(shader, infoLen, NULL, buf);
+          LOGE("Could not compile shader %d:\n%s\n", shaderType, buf);
+          free(buf);
+        }
+        glDeleteShader(shader);
+        shader = 0;
+      }
+    }
+  }
+  return shader;
+}
+
+GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
+  GLuint vertexShader = loadShader(GL_VERTEX_SHADER, pVertexSource);
+  if (!vertexShader) {
+    return 0;
+  }
+
+  GLuint pixelShader = loadShader(GL_FRAGMENT_SHADER, pFragmentSource);
+  if (!pixelShader) {
+    return 0;
+  }
+
+  GLuint program = glCreateProgram();
+  if (program) {
+    glAttachShader(program, vertexShader);
+    checkGlError("glAttachShader");
+    glAttachShader(program, pixelShader);
+    checkGlError("glAttachShader");
+    glLinkProgram(program);
+    GLint linkStatus = GL_FALSE;
+    glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+    if (linkStatus != GL_TRUE) {
+      GLint bufLength = 0;
+      glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
+      if (bufLength) {
+        char* buf = (char*)malloc(bufLength);
+        if (buf) {
+          glGetProgramInfoLog(program, bufLength, NULL, buf);
+          LOGE("Could not link program:\n%s\n", buf);
+          free(buf);
+        }
+      }
+      glDeleteProgram(program);
+      program = 0;
+    }
+  }
+  return program;
+}
+
+GLuint gProgram;
+GLuint gvPositionHandle;
+
+bool setupGraphics(int w, int h) {
+  printGLString("Version", GL_VERSION);
+  printGLString("Vendor", GL_VENDOR);
+  printGLString("Renderer", GL_RENDERER);
+  printGLString("Extensions", GL_EXTENSIONS);
+
+  LOGI("setupGraphics(%d, %d)", w, h);
+  gProgram = createProgram(gVertexShader, gFragmentShader);
+  if (!gProgram) {
+    LOGE("Could not create program.");
+    return false;
+  }
+  gvPositionHandle = glGetAttribLocation(gProgram, "aPos");
+  checkGlError("glGetAttribLocation");
+  LOGI("glGetAttribLocation(\"aPos\") = %d\n", gvPositionHandle);
+
+  glViewport(0, 0, w, h);
+  checkGlError("glViewport");
+  return true;
+}
+
+const GLfloat gTriangleVertices[] = {         0.5f,  0.5f,      1.0f, 1.0f, // top right
+                                              0.5f, -0.5f,     1.0f, 0.0f, // bottom right
+                                              -0.5f, -0.5f,      0.0f, 0.0f, // bottom left
+                                              -0.5f,  0.5f,      0.0f, 1.0f  // top left
+};
+unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+};
+void renderFrame() {
+  static float grey;
+  grey += 0.01f;
+  if (grey > 1.0f) {
+    grey = 0.0f;
+  }
+  GLuint VBO, VAO, EBO; //顶点缓冲对象、顶点数组对象、元素缓冲对象
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  //将新创建的缓冲绑定到GL_ARRAY_BUFFER
+  glBufferData(GL_ARRAY_BUFFER, sizeof(gTriangleVertices), gTriangleVertices, GL_STATIC_DRAW);//调用glBufferData函数，它会把之前定义的顶点数据复制到缓冲的内存中：（目标缓冲类型、传输数据字节大小、期待发送的实际数据、如何管理给定数据）
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//和VBO类似，我们会把这些函数调用放在绑定和解绑函数调用之间
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  //解析顶点数据
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void * ) 0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void * ) (3 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D,texture);
+  // 为当前绑定的纹理对象设置环绕、过滤方式
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char *data = stbi_load(("/Users/tanyubo/Desktop/car_image.png"), &width, &height, &nrChannels, 0);
+  if (data)
+  {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    LOGE("Failed to load texture");
+  }
+  stbi_image_free(data);
+//  glBindTexture();
+  glClearColor(grey, grey, grey, 1.0f);
+  checkGlError("glClearColor");
+  glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+  checkGlError("glClear");
+
+  glUseProgram(gProgram);
+  checkGlError("glUseProgram");
+
+  glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0,
+                        gTriangleVertices);
+  checkGlError("glVertexAttribPointer");
+  glEnableVertexAttribArray(gvPositionHandle);
+  checkGlError("glEnableVertexAttribArray");
+//  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices);
+  checkGlError("glDrawArrays");
+}
+
+extern "C" {
+JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv* env,
+                                                              jobject obj,
+                                                              jint width,
+                                                              jint height);
+JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_step(JNIEnv* env,
+                                                              jobject obj);
+};
+
+JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv* env,
+                                                              jobject obj,
+                                                              jint width,
+                                                              jint height) {
+  setupGraphics(width, height);
+}
+
+JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_step(JNIEnv* env,
+                                                              jobject obj) {
+  renderFrame();
+}
